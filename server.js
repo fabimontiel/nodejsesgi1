f02de7e256a63cccc6b5da06694b99685595bc37
fabiana montiel <fabimontiel@gmail.com>
2019-09-23 11:47:07 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-print("Hello World")
+// console.log('Hello World');
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
