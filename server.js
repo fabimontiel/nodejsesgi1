1b76198b86b25561d99e2390f72ea1f254d634fd
fabiana montiel <fabimontiel@gmail.com>
2019-09-23 14:33:29 +0200

    exo 3

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-// console.log('Hello World');
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -11,7 +12,20 @@ app.get('/', (req, res) => res.send('Hello World!'))
 })*/
 
 app.post('/chat', function (req, res){
-  res.send('Nous sommes a Paris');
+  const message = req.body.msg
+  if(message == "ville"){
+    res.send("Nous sommes a Paris")
+  }else if(message == "meteo"){
+    res.send("Il fait beau")
+  }
 });
 
-app.listen(port, () => console.log('Example app listening on port ${port}!'))
+app.get('/hello', (req, res) => {
+ if (req.query.nom == null) {
+   res.send("Quel est votre nom ?");
+ } else {
+   res.send("Bonjour, " + req.query.nom + " !");
+ }}
+)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
